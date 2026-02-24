fc5bcf248a522a9ad660f05eaa75829e2058ad23
jistbinw3 <jistbinw3@mgial.com>
2026-02-24 06:18:37 -0700

    database teacher provided

--- database.py
+++ database.py
@@ -9,7 +9,7 @@ load_dotenv()
 def get_db_connection():
     try:
         connection = mysql.connector.connect(
-            host=os.getenv("MYSQL_HOST", "127.0.0.1"),
+            host=os.getenv("MYSQL_HOST"),
             port=int(os.getenv("MYSQL_PORT", "3306")),
             user=os.getenv("MYSQL_USER"),
             password=os.getenv("MYSQL_PASSWORD"),
